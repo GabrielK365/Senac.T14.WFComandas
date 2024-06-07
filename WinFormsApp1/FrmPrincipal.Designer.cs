305f71f006a4be8536eafcde2b82af2c46d76697
Gabriel Kessler <gabrielkessler2008@gmail.com>
2024-06-06 21:32:26 -0300

    Criada a navegação das telas

--- WinFormsApp1/FrmPrincipal.Designer.cs
+++ WinFormsApp1/FrmPrincipal.Designer.cs
@@ -39,22 +39,22 @@
             // groupBox1
             // 
             groupBox1.BackColor = Color.Transparent;
-            groupBox1.BackGColor = Color.AliceBlue;
+            groupBox1.BackGColor = Color.Blue;
             groupBox1.BaseColor = Color.Transparent;
-            groupBox1.BorderColorG = Color.FromArgb(159, 159, 161);
-            groupBox1.BorderColorH = Color.FromArgb(182, 180, 186);
+            groupBox1.BorderColorG = SystemColors.ActiveCaptionText;
+            groupBox1.BorderColorH = SystemColors.ActiveCaptionText;
             groupBox1.Controls.Add(Comanda);
             groupBox1.Controls.Add(Usuarios);
             groupBox1.Controls.Add(Pedido);
             groupBox1.Controls.Add(Cardapio);
             groupBox1.Font = new Font("Tahoma", 9F);
             groupBox1.ForeColor = Color.FromArgb(53, 53, 53);
-            groupBox1.HeaderColor = Color.Transparent;
-            groupBox1.Location = new Point(39, 88);
+            groupBox1.HeaderColor = SystemColors.ButtonShadow;
+            groupBox1.Location = new Point(138, 308);
             groupBox1.MinimumSize = new Size(136, 50);
             groupBox1.Name = "groupBox1";
             groupBox1.Padding = new Padding(5, 28, 5, 5);
-            groupBox1.Size = new Size(715, 289);
+            groupBox1.Size = new Size(780, 290);
             groupBox1.SmoothingType = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
             groupBox1.TabIndex = 1;
             // 
@@ -64,13 +64,14 @@
             Comanda.Customization = "7e3t//Ly8v/r6+v/5ubm/+vr6//f39//p6en/zw8PP8UFBT/gICA/w==";
             Comanda.Font = new Font("Segoe UI", 9F);
             Comanda.Image = null;
-            Comanda.Location = new Point(197, 81);
+            Comanda.Location = new Point(218, 92);
             Comanda.Name = "Comanda";
             Comanda.NoRounding = false;
             Comanda.Size = new Size(146, 137);
             Comanda.TabIndex = 4;
             Comanda.Text = "Comanda";
             Comanda.Transparent = false;
+            Comanda.Click += Pedido_Click;
             // 
             // Usuarios
             // 
@@ -78,13 +79,14 @@
             Usuarios.Customization = "7e3t//Ly8v/r6+v/5ubm/+vr6//f39//p6en/zw8PP8UFBT/gICA/w==";
             Usuarios.Font = new Font("Segoe UI", 9F);
             Usuarios.Image = null;
-            Usuarios.Location = new Point(537, 81);
+            Usuarios.Location = new Point(582, 92);
             Usuarios.Name = "Usuarios";
             Usuarios.NoRounding = false;
             Usuarios.Size = new Size(146, 137);
             Usuarios.TabIndex = 3;
             Usuarios.Text = "Usuarios";
             Usuarios.Transparent = false;
+            Usuarios.Click += Usuarios_Click;
             // 
             // Pedido
             // 
@@ -92,13 +94,14 @@
             Pedido.Customization = "7e3t//Ly8v/r6+v/5ubm/+vr6//f39//p6en/zw8PP8UFBT/gICA/w==";
             Pedido.Font = new Font("Segoe UI", 9F);
             Pedido.Image = null;
-            Pedido.Location = new Point(368, 81);
+            Pedido.Location = new Point(401, 92);
             Pedido.Name = "Pedido";
             Pedido.NoRounding = false;
             Pedido.Size = new Size(146, 137);
             Pedido.TabIndex = 2;
-            Pedido.Text = "Pedido";
+            Pedido.Text = "Pedido Cozinha";
             Pedido.Transparent = false;
+            Pedido.Click += Pedido_Click;
             // 
             // Cardapio
             // 
@@ -106,24 +109,25 @@
             Cardapio.Customization = "7e3t//Ly8v/r6+v/5ubm/+vr6//f39//p6en/zw8PP8UFBT/gICA/w==";
             Cardapio.Font = new Font("Segoe UI", 9F);
             Cardapio.Image = null;
-            Cardapio.Location = new Point(30, 81);
+            Cardapio.Location = new Point(34, 92);
             Cardapio.Name = "Cardapio";
             Cardapio.NoRounding = false;
             Cardapio.Size = new Size(146, 137);
             Cardapio.TabIndex = 1;
             Cardapio.Text = "Cardapio";
             Cardapio.Transparent = false;
+            Cardapio.Click += Cardapio_Click;
             // 
             // FrmPrincipal
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.Control;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1067, 660);
             Controls.Add(groupBox1);
             ForeColor = SystemColors.ControlLightLight;
             Name = "FrmPrincipal";
-            Text = "Form1";
+            Text = "Comanda";
             groupBox1.ResumeLayout(false);
             ResumeLayout(false);
         }
--- WinFormsApp1/FrmPrincipal.cs
+++ WinFormsApp1/FrmPrincipal.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography;
+
 namespace WinFormsApp1
 {
     public partial class FrmPrincipal : Form
@@ -6,5 +8,26 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+
+        private void Cardapio_Click(object sender, EventArgs e)
+        {
+            new frmCardapio().ShowDialog();
+        }
+
+        private void Comanda_Click(object sender, EventArgs e)
+        {
+            new FrmComanda().ShowDialog();
+        }
+
+        private void Usuarios_Click(object sender, EventArgs e)
+        {
+            new FrmUsuarios().ShowDialog();
+        }
+
+        private void Pedido_Click(object sender, EventArgs e)
+        {
+            new frmPedidoCozinha().ShowDialog();
+        }
+
     }
 }
